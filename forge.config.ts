ed9b61e0aa25472190a8fee03115a6156f006d61
Sebastian Mahr <sebastian.mahr@js-soft.com>
2025-04-09 08:06:38 +0200

    chore: fix release

--- forge.config.ts
+++ forge.config.ts
@@ -6,7 +6,6 @@ import { MakerRpm } from "@electron-forge/maker-rpm";
 import { VitePlugin } from "@electron-forge/plugin-vite";
 import { FusesPlugin } from "@electron-forge/plugin-fuses";
 import { FuseV1Options, FuseVersion } from "@electron/fuses";
-import PublisherERS from "./publisher/PublisherERS";
 import { config as dotenv } from "dotenv";
 dotenv();
 const config: ForgeConfig = {
@@ -57,11 +56,14 @@ const config: ForgeConfig = {
     }),
   ],
   publishers: [
-    new PublisherERS({
-      baseUrl: "https://update.sebmahr.de",
-      username: "sebbi",
-      password: process.env.PASSWORD || "",
-    }),
+    {
+      name: './publisher/PublisherERS',
+      config: {
+        baseUrl: "https://update.sebmahr.de",
+        username: "sebbi",
+        password: process.env.PASSWORD || "",
+      }
+    }
   ],
 };
 
--- publisher/PublisherERS.ts
+++ publisher/PublisherERS.ts
@@ -1,15 +1,15 @@
-import path from "path";
-
-import { PublisherBase, PublisherOptions } from "@electron-forge/publisher-base";
+import {
+  PublisherBase,
+  PublisherOptions,
+} from "@electron-forge/publisher-base";
 import { ForgeArch, ForgePlatform } from "@electron-forge/shared-types";
 import debug from "debug";
 import FormData from "form-data";
-import fs from "fs-extra";
-
-import fetch, { RequestInfo, RequestInit, Response } from "node-fetch";
+import * as fs from "fs-extra";
+import axios, { AxiosRequestConfig } from "axios";
 
 import { PublisherERSConfig } from "./Config";
-
+import { basename } from "node:path";
 
 const d = debug("electron-forge:publish:ers");
 
@@ -22,16 +22,22 @@ interface ERSVersion {
   flavor: { name: string };
 }
 
-const fetchAndCheckStatus = async (url: RequestInfo, init?: RequestInit): Promise<Response> => {
-  const result = await fetch(url, init);
-  if (result.ok) {
+async function fetchAndCheckStatus<T>(url: string,
+  init?: AxiosRequestConfig): Promise<T> {
+  const result = await axios(url, init);
+  if (result.status >= 200 && result.status < 300) {
     // res.status >= 200 && res.status < 300
-    return result;
+    return result.data;
   }
-  throw new Error(`ERS publish failed with status code: ${result.status} (${result.url})`);
-};
+  throw new Error(
+    `ERS publish failed with status code: ${result.status} (${url})`
+  );
+}
 
-export const ersPlatform = (platform: ForgePlatform, arch: ForgeArch): string => {
+export const ersPlatform = (
+  platform: ForgePlatform,
+  arch: ForgeArch
+): string => {
   switch (platform) {
     case "darwin":
       return "osx_64";
@@ -47,12 +53,15 @@ export const ersPlatform = (platform: ForgePlatform, arch: ForgeArch): string =>
 export default class PublisherERS extends PublisherBase<PublisherERSConfig> {
   name = "electron-release-server";
 
-  async publish({ makeResults, setStatusLine }: PublisherOptions): Promise<void> {
+  async publish({
+    makeResults,
+    setStatusLine,
+  }: PublisherOptions): Promise<void> {
     const { config } = this;
 
     if (!(config.baseUrl && config.username && config.password)) {
       throw new Error(
-        "In order to publish to ERS you must set the \"electronReleaseServer.baseUrl\", \"electronReleaseServer.username\" and \"electronReleaseServer.password\" properties in your Forge config. See the docs for more info"
+        'In order to publish to ERS you must set the "electronReleaseServer.baseUrl", "electronReleaseServer.username" and "electronReleaseServer.password" properties in your Forge config. See the docs for more info'
       );
     }
 
@@ -60,34 +69,45 @@ export default class PublisherERS extends PublisherBase<PublisherERSConfig> {
 
     const api = (apiPath: string) => `${config.baseUrl}/${apiPath}`;
 
-    const { token } = (await (
-      await fetchAndCheckStatus(api("api/auth/login"), {
+    const { token } = await await fetchAndCheckStatus<{ token: string }>(
+      api("api/auth/login"),
+      {
         method: "POST",
-        body: JSON.stringify({
+        data: JSON.stringify({
           username: config.username,
-          password: config.password
+          password: config.password,
         }),
         headers: {
-          "Content-Type": "application/json"
-        }
-      })
-    ).json()) as { token: string };
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
-    const authFetch = (apiPath: string, options?: RequestInit) =>
-      fetchAndCheckStatus(api(apiPath), {
+    function authFetch<T>(apiPath: string, options?: AxiosRequestConfig) {
+      return fetchAndCheckStatus<T>(api(apiPath), {
         ...(options || {}),
-        headers: { ...(options || {}).headers, Authorization: `Bearer ${token}` }
+        headers: {
+          ...(options || {}).headers,
+          Authorization: `Bearer ${token}`,
+        },
       });
+    }
 
     const flavor = config.flavor || "default";
 
     for (const makeResult of makeResults) {
       const { packageJSON } = makeResult;
-      const artifacts = makeResult.artifacts.filter((artifactPath) => path.basename(artifactPath).toLowerCase() !== "releases");
+      const artifacts = makeResult.artifacts.filter(
+        (artifactPath) => basename(artifactPath).toLowerCase() !== "releases"
+      );
 
-      const versions: ERSVersion[] = (await (await authFetch("api/version")).json()) as ERSVersion[];
+      const versions = (await (
+        await authFetch<ERSVersion[]>("api/version")
+      )) ;
       const existingVersion = versions.find((version) => {
-        return version.name === packageJSON.version && version.flavor.name === flavor;
+        return (
+          version.name === packageJSON.version && version.flavor.name === flavor
+        );
       });
 
       let channel = "stable";
@@ -104,28 +124,35 @@ export default class PublisherERS extends PublisherBase<PublisherERSConfig> {
       if (!existingVersion) {
         await authFetch("api/version", {
           method: "POST",
-          body: JSON.stringify({
+          data: JSON.stringify({
             channel: channel,
             flavor: flavor,
             name: packageJSON.version,
             notes: "",
-            id: packageJSON.version + "_" + channel
+            id: packageJSON.version + "_" + channel,
           }),
           headers: {
-            "Content-Type": "application/json"
-          }
+            "Content-Type": "application/json",
+          },
         });
       }
 
       let uploaded = 0;
-      const updateStatusLine = () => setStatusLine(`Uploading distributable (${uploaded}/${artifacts.length})`);
+      const updateStatusLine = () =>
+        setStatusLine(
+          `Uploading distributable (${uploaded}/${artifacts.length})`
+        );
       updateStatusLine();
 
       await Promise.all(
         artifacts.map(async (artifactPath: string) => {
           const platform = ersPlatform(makeResult.platform, makeResult.arch);
           if (existingVersion) {
-            const existingAsset = existingVersion.assets.find((asset) => asset.name === path.basename(artifactPath) && asset.platform === platform);
+            const existingAsset = existingVersion.assets.find(
+              (asset) =>
+                asset.name === basename(artifactPath) &&
+                asset.platform === platform
+            );
 
             if (existingAsset) {
               d("asset at path:", artifactPath, "already exists on server");
@@ -142,14 +169,18 @@ export default class PublisherERS extends PublisherBase<PublisherERSConfig> {
 
           // see https://github.com/form-data/form-data/issues/426
           const fileOptions = {
-            knownLength: fs.statSync(artifactPath).size
+            knownLength: fs.statSync(artifactPath).size,
           };
-          artifactForm.append("file", fs.createReadStream(artifactPath), fileOptions);
+          artifactForm.append(
+            "file",
+            fs.createReadStream(artifactPath),
+            fileOptions
+          );
 
           await authFetch("api/asset", {
             method: "POST",
-            body: artifactForm,
-            headers: artifactForm.getHeaders()
+            data: artifactForm,
+            headers: artifactForm.getHeaders(),
           });
           d("upload successful for asset:", artifactPath);
           uploaded += 1;
@@ -159,5 +190,3 @@ export default class PublisherERS extends PublisherBase<PublisherERSConfig> {
     }
   }
 }
-
-export { PublisherERS, PublisherERSConfig };
